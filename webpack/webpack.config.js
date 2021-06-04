fad48faa037cafcbe9bbd22009ac63b3e407269f
Dmitry Yadrikhinsky <dyadrihinsky@i-neti.ru>
2021-06-04 21:08:42 +0300

    Init

---
+++ webpack/webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.ts',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: '[name].js',
+    // publicPath: path.resolve(__dirname, 'src', 'assets'),
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+    alias: {
+      alias: path.resolve(__dirname, '../src/alias'),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        // TODO
+        test: /\.(png|svg|ttf|eot|woff2?)/,
+        loader: 'file-loader',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      inject: 'body',
+    }),
+    new CopyPlugin({
+      patterns: [{ from: path.resolve('src', 'assets'), to: 'assets' }],
+    }),
+  ],
+};
